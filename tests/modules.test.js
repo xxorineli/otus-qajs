a89daaf834570a9702c3d99b6874f79f3287a512
Eli Sorin <xxorineli@gmail.com>
2024-03-12 08:16:14 +0300

    upd

---
+++ tests/modules.test.js
@@ -0,0 +1,30 @@
+import {farewell, greet} from '../src/modules.js'
+
+describe('greet', () => {
+  // For TDD approach
+  // it('is function', () => {
+  //   expect(typeof greet).toBe('function')
+  // })
+  
+  it('should greet Eli', () => {
+    // Using Arrange-Act-Assert pattern
+    const username = 'Eli' // arrange
+    const result = greet(username) // act
+    expect(result).toBe('Hello, Eli') // assert
+  })
+
+  it('should work correct with empty string', () => {
+    expect(greet('')).toBe('Hello, ')
+  })
+})
+
+
+describe('farewell', () => {
+  it('should farewell Eli', () => {
+    expect(farewell('Eli')).toBe('Goodbye, Eli')
+  })
+
+  it('should work correct with empty string', () => {
+    expect(farewell('')).toBe('Goodbye, ')
+  })
+})
